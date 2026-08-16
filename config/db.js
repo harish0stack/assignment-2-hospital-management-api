bd755dea73a4ed142d8e7ccee680ac50528a59b1
Harish <harishmalviya057@gmail.com>
2026-08-17 00:56:27 +0530

    Change MongoDB connection string password to placeholder
    Updated MongoDB connection string to use a placeholder for the password.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb+srv://harishmalviya511_db_user:t9Gy1yB0H5aK6yID@cluster0.dxfzto2.mongodb.net/?appName=Cluster0");
+mongoose.connect("mongodb+srv://harishmalviya511_db_user:<db:password>@cluster0.dxfzto2.mongodb.net/?appName=Cluster0");
 
 const db = mongoose.connection;
 
